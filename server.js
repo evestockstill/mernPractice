5a80fcf8fe110ed57fa56dec822c9c9872d81b54
Eve Stockstill <evestockstillpdx@gmail.com>
2020-03-20 14:34:48 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
